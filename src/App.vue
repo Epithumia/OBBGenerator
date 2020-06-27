65b97ac549e6c98d6a5c3d4c87645b1648cf26f8
Rafael Lopez <lopez@airelle.info>
2020-06-28 01:44:58 +0200

    Mise en place et test de base.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <o-b-b-generator />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OBBGenerator from './components/OBBGenerator'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    OBBGenerator
   }
 }
 </script>
---
+++ src/components/OBBGenerator.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <icon-base><icon-circle /></icon-base>
+        <p>
+            You can use whatever icon you want and keep the base consistent with slots
+        </p>
+        <select></select>
+    </div>
+</template>
+
+<script>
+    import IconBase from './IconBase.vue'
+    import IconCircle from "@/components/IconCircle";
+
+    export default {
+        components: {
+            IconCircle,
+            IconBase
+        }
+    }
+</script>
+
+
